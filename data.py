336f5180abf509ddffb9336fa424215699e18a8d
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2020-10-04 18:45:50 -0400

    data merge

--- data.py
+++ data.py
@@ -2,3 +2,21 @@ import pandas as pd
 
 
 hosp = pd.read_csv("hospitalization.csv")
+hosp = hosp[["open_covid_region_code", "date", "hospitalized_new"]]
+
+hosp["date"] = pd.to_datetime(hosp["date"], format="%Y-%m-%d") - pd.to_timedelta(
+    7, unit="d"
+)
+hosp = (
+    hosp.groupby(
+        ["open_covid_region_code", pd.Grouper(key="date", freq="W-MON", closed="left")]
+    )
+    .sum()
+    .reset_index()
+)
+
+search = pd.read_csv("2020_US_weekly_symptoms_dataset.csv")
+search["date"] = pd.to_datetime(search["date"], format="%Y-%m-%d")
+
+merged = pd.merge(search, hosp, how="left", on=["open_covid_region_code", "date"])
+merged.to_csv("merge.csv")
