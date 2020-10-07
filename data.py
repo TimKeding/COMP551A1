4f5199ab12a827fdb24aec584eab25672e14deed
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2020-10-07 15:13:35 -0400

    add method to remove regions with little data add method to fill NaN with values. rename methods

--- data.py
+++ data.py
@@ -3,18 +3,18 @@ import pandas as pd
 
 
 class Data:
-
     def __init__(self):
-        self.data = self.get_data()
+        self.d = Data.import_data()
         self.filter_out_zeros()
 
-    def get_data(self):
+    @staticmethod
+    def import_data():
         hosp = pd.read_csv("hospitalization.csv")
         hosp = hosp[["open_covid_region_code", "date", "hospitalized_new"]]
 
-        hosp["date"] = pd.to_datetime(hosp["date"], format="%Y-%m-%d") - pd.to_timedelta(
-            7, unit="d"
-        )
+        hosp["date"] = pd.to_datetime(
+            hosp["date"], format="%Y-%m-%d"
+        ) - pd.to_timedelta(7, unit="d")
         hosp = (
             hosp.groupby(
                 [
@@ -22,42 +22,50 @@ class Data:
                     pd.Grouper(key="date", freq="W-MON", closed="left"),
                 ]
             )
-                .sum()
-                .reset_index()
+            .sum()
+            .reset_index()
         )
 
         search = pd.read_csv("2020_US_weekly_symptoms_dataset.csv")
         search["date"] = pd.to_datetime(search["date"], format="%Y-%m-%d")
 
-        results = pd.merge(search, hosp, how="left", on=["open_covid_region_code", "date"])
-        return results.fillna(0)
+        results = pd.merge(
+            search, hosp, how="left", on=["open_covid_region_code", "date"]
+        )
+        return results
 
-    def filter_regions_count(self, threshold=0.1):
+    def fill_na(self, val):
+        self.d.fillna(val)
+
+    def filter_low_data_regions(self, threshold=0.1):
         """
         Keep all open_covid_region_code regions that have > 0.1 * #_elements that are not
         NaN values.
-        :param d: region data
         :param threshold: cutoff point. Any region with less than 0.1 of all cells filled
                 with data are filtered out
         """
         data_per_region = (
-            self.data.groupby(["open_covid_region_code"])
-                .count()
-                .loc[:, "symptom:Adrenal crisis":"symptom:Yawn"]
-                .sum(axis=1)
+            self.d.groupby(["open_covid_region_code"])
+            .count()
+            .loc[:, "symptom:Adrenal crisis":"symptom:Yawn"]
+            .sum(axis=1)
         )
-        size_per_region = self.data.groupby(["open_covid_region_code"]).size()[0] * self.data.shape[1]
+        size_per_region = (
+            self.d.groupby(["open_covid_region_code"]).size()[0] * self.d.shape[1]
+        )
         good_regions = list(
             data_per_region[data_per_region > threshold * size_per_region].index
         )
-        return self.data[self.data["open_covid_region_code"].isin(good_regions)].reset_index(drop=True)
+        return self.d[self.d["open_covid_region_code"].isin(good_regions)].reset_index(
+            drop=True
+        )
 
     def filter_out_zeros(self):
-        return self.data.dropna(axis=1, how="all")
+        return self.d.dropna(axis=1, how="all")
 
     def keep_15_symptoms(self):
         # sum all columns(symptoms)
-        trim_data = copy.deepcopy(self.data)
+        trim_data = copy.deepcopy(self.d)
         cropped = trim_data.loc[:, "symptom:Adrenal crisis":"symptom:Yawn"]
         s = cropped.sum(axis=0)
 
@@ -79,4 +87,4 @@ class Data:
         )
 
     def merge_regions(self):
-        return self.data.groupby(["date"]).sum().reset_index()
+        return self.d.groupby(["date"]).sum().reset_index()
