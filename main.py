3a76009083a2b6a2f1840cf8753928ed2d9cbe79
Saksham171199 <saksham.mungroo@mail.mcgill.ca>
2020-10-07 18:23:58 -0400

    -Symptoms Popularity graph done

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from data import Data
-from pca import PCAPlotting
 from kmeans import Kmeans
+from pca import PCAPlotting
+import symptomsPopularity
 
 dataset = Data()
-trimmed_dataset = dataset.keep_15_symptoms()
-pca_data = PCAPlotting(trimmed_dataset)
+dataset.keep_15_symptoms()
+dataset.fill_na(0)
+pca_data = PCAPlotting(dataset.d)
 pca_data.reduce_dimensionality(3)
 kmeans = Kmeans(pca_data.reduced_data)
 kmeans.find_clusters(5)
 kmeans.plot_kmeans()
+dataset.merge_regions()
+
+sym_pop = symptomsPopularity.SymptomPopularity(dataset.d)
+sym_pop.symptoms_popularity()
+
+
+
