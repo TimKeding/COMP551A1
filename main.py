f97ef3c071c62f1b92c7ab5e3a8d2f3d5cb9951f
Timbermaw <kedingt@gmail.com>
2020-10-07 12:43:52 -0400

    Added kmeans clustering
    Added kmeans clustering allowing use to cluster the visualized PCA data

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from data import Data
 from pca import PCAPlotting
+from kmeans import Kmeans
 
 dataset = Data()
 trimmed_dataset = dataset.keep_15_symptoms()
 pca_data = PCAPlotting(trimmed_dataset)
-pca_data.reduce_dimensionality(2)
-pca_data.plot_data()
+pca_data.reduce_dimensionality(3)
+kmeans = Kmeans(pca_data.reduced_data)
+kmeans.find_clusters(5)
+kmeans.plot_kmeans()
